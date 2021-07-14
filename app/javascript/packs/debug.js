c7982e33adf4eda31c5594980f6cda91bea95e68
小野隆規 <petite@PetiteEtoile.local>
2021-07-14 21:40:29 +0900

    debugを無効化

--- app/javascript/packs/debug.js
+++ app/javascript/packs/debug.js
@@ -1,9 +1,9 @@
 
 function debug(obj){
     if(obj == null){
-        console.log("null")
+        // console.log("null")
     }else{
-        console.log(JSON.parse(JSON.stringify(obj)))
+        // console.log(JSON.parse(JSON.stringify(obj)))
     }
 }
 
